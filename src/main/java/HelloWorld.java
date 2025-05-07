f5564bf0ffdda5789645463457e7b6634e1ef51a
Filippo Pracucci <filippo.pracucci@studio.unibo.it>
2025-05-07 13:12:48 +0200

    enable scala plugin and add scala source

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -3,7 +3,13 @@
  */
 public class HelloWorld {
 
+    private static final String HELLO_WORLD = "Hello, world!";
+
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        System.out.println(HELLO_WORLD);
+    }
+
+    public static void greet(final String name) {
+        System.out.println("Hello " + name);
     }
 }
---
+++ src/main/scala/ScalaExample.scala
@@ -0,0 +1,5 @@
+import HelloWorld.greet
+
+object ScalaExample extends App:
+
+  greet("John")
