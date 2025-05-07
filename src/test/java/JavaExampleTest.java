021fe5b417c5a639c8863b50b29ee903a4e35166
Filippo Pracucci <filippo.pracucci@studio.unibo.it>
2025-05-07 13:21:29 +0200

    gradle test configuration and add java, scala sources

---
+++ src/test/java/JavaExampleTest.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class JavaExampleTest {
+
+    @Test
+    void test() {
+        assertTrue(true);
+        assertFalse(false);
+    }
+
+}
---
+++ src/test/scala/ScalaExampleTest.scala
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class ScalaExampleTest:
+
+  @Test
+  def test: Unit =
+    assertTrue(true)
+    assertFalse(false)
